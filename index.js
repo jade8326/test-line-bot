13583e9122e1090b81af3489fafdc3c3d6516ddc
Jessada Tritipvithaya <jessadat@ais.co.th>
2018-08-26 21:29:42 +0700

    remove bodyparser

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const client = new line.Client(config);
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
